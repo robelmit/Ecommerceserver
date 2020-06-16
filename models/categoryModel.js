44d8e90f55e4a132475ddbcf69e0b531efd32ac5
dehacker <robitsegay2018@gmail.com>
2020-06-16 01:41:55 -0700

    Finishing all database Models

---
+++ models/categoryModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+});
+// categorySchema.virtual('id').get(function () {
+//     return this._id.toHexString();
+// });
+
+// categorySchema.set('toJSON', {
+//     virtuals: true,
+// });
+
+const Category = mongoose.model("Category", categorySchema);
+
+export default Category;
---
+++ models/productsModel.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  richDescription: {
+    type: String,
+    default: "",
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  images: [
+    {
+      type: String,
+    },
+  ],
+  brand: {
+    type: String,
+    default: "",
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  numReviews: {
+    type: Number,
+    default: 0,
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+ProductSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+ProductSchema.set("toJSON", {
+  virtuals: true,
+});
+
+const Products = mongoose.model("Products", ProductSchema);
+
+export default Products;
